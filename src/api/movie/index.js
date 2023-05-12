d490ecc44e85d01686a182c4c093e0ea2c6aed33
avijeetnair <avijeetnair.avj@gmail.com>
2023-05-13 04:29:42 +0530

    Completed Client and Admin Dashboard

--- src/api/movie/index.js
+++ src/api/movie/index.js
@@ -1,15 +1,14 @@
-import { AxiosInstance } from "../../util/AxiosInstance";
-export const getAllMovies = async() => {
+import { AxiosInstance } from '../../util/AxiosInstance'
+export const getAllMovies = async () => {
 
     const URL = 'mba/api/movies'
 
-    try{
-        const response = await AxiosInstance.get(URL);
+    try {
+        const response = await AxiosInstance.get(URL)
 
-        return response;
-    }catch(error){
-        console.log(error);
-        throw error;
+        return response
+    } catch (error) {
+        console.log(error)
+        throw error
     }
-
 }
--- src/components/routes/AppRoutes.js
+++ src/components/routes/AppRoutes.js
@@ -1,22 +1,36 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import LandingPage from "../LandindPage/LandingPage";
-import Login from "../Auth/Auth";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import LandingPage from '../LandingPage/LandingPage.js'
+import Login from '../Auth/Auth'
+import Admin from '../Admin/Admin'
+import Client from '../Client/Client'
+
 const AppRoutes = () => {
     return (
         <Router>
             <Routes>
                 <Route
-                exact path = "/"
-                element = {<LandingPage/>}
+                    exact
+                    path='/'
+                    element={<LandingPage />}
                 />
-            <Route
+                <Route
                     exact
                     path='/login'
                     element={<Login />}
                 />
+                <Route
+                    exact
+                    path='/admin'
+                    element={<Admin />}
+                />
+                <Route
+                    exact
+                    path='/client'
+                    element={<Client />}
+                />
             </Routes>
         </Router>
     )
-};
+}
 
-export default AppRoutes;
+export default AppRoutes
